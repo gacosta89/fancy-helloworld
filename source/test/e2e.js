cfe8d3882974f05ada25fa80dd1768dcbfd0acb3
gacosta89 <gacosta.r89@gmail.com>
2017-08-02 00:43:54 -0300

    Remove old e2e file with nightwatch-autorun
    e2e tests are handled by the e2e script now

--- source/test/e2e.js
+++
@@ -1,8 +0,0 @@
-import runner from 'nightwatch-autorun';
-import makeApp from 'server/app.js';
-import config from 'config';
-
-const app = makeApp();
-const NODE_PORT = process.env.NODE_PORT || config.get('port');
-
-runner({port: NODE_PORT, server: app});
